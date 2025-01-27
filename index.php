ca737a7a46169f477934318951e9aa9a97f370f4
Filippo-Makai <filippo.giacomello@wearemakai.com>
2025-01-27 16:56:12 +0100

    test

--- index.php
+++ index.php
@@ -14,3 +14,16 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 ?>
+<!DOCTYPE html>
+<html lang="it-IT">
+    <head>
+        <title>Mappa Fiera Agricola Tech</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+        <script src="js/script.js"></script>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+
+    </body>
+    </html>
