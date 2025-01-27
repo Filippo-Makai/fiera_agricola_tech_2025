0381ad42f49a8e92aa4afeb4f4e753f4f0798940
Filippo-Makai <filippo.giacomello@wearemakai.com>
2025-01-27 16:54:27 +0100

    connect

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 $servername = "fiera-agricola-tech-2025-4mcqc-mysql.fiera-agricola-tech-2025-4mcqc.svc.cluster.local";
 $username = "makai";
 $password = "sV8[pI7:jZ7*zK8[";
+$dbname = "fiera-agricola-tech-2025";
+$port = "3306";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
